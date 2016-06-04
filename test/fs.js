3cb154a77a4e8211e1386ac81634e516427c9503
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2016-06-03 18:36:57 -0700

    Allow adding multiple markers at one time. Add tests for multiple marker correctness. Move test marker definitions to a separate file.

--- test/fs.js
+++ test/fs.js
@@ -12,6 +12,7 @@ var File = require('vinyl');
 var should = require('should');
 var assert = require('assert');
 var concatStream = require('concat-stream');
+var markerDefinitions = require('./fixtures/marker-definitions');
 
 // run a buffer/stream-independent test
 runCoreTest();
@@ -20,160 +21,6 @@ runCoreTest();
 runTests('buffer');
 runTests('stream');
 
-
-
-var markerDefinitions = [
-    {
-        // a simple replacement of the marker.
-        tag: 'html-copyright',
-        re: '<!-- insert:html-copyright:(\\d{4}) -->',
-        replace: function (context, match, startYear) {
-            var year = new Date().getFullYear();
-            return '<!-- Copyright BAM ' + startYear + '-' + year + ' -->';
-        },
-        opts: {data: {expected: {files: 1, matches: 1}}}
-    }, {
-        tag: 'html-copyright-via-regex',
-        re: new RegExp('<!-- insert:html-copyright-via-regex:(\\d{4}) -->'),
-        replace: function (context, match, startYear) {
-            var year = new Date().getFullYear();
-            return '<!-- Copyright BAM ' + startYear + '-' + year + ' -->';
-        },
-        opts: {data: {expected: {files: 1, matches: 1}}}
-    }, {
-        // insert a single item using a string replacement.
-        tag: 'html-block-string-css',
-        //    1     2        3                                     x          x        4      5x       x
-        re: '(\\n?)([ \\t]*)(<!-- @begin:html-block-string-css -->(?:[ \\t]*)(?:\\n?))([^]*?)((?:\\n?)(?:[ \\t]*)<!-- @end:html-block-string-css -->)',
-        replace: '$1$2$3$2<link rel="stylesheet" href="css/combined.css">$5',
-        opts: {data: {expected: {files: 1, matches: 1}}}
-    }, {
-        // insert multiple items using a replacement function.
-        tag: 'html-block-func-css',
-        //    1     2        3                                   x          x        4      5x       x
-        re: '(\\n?)([ \\t]*)(<!-- @begin:html-block-func-css -->(?:[ \\t]*)(?:\\n?))([^]*?)((?:\\n?)(?:[ \\t]*)<!-- @end:html-block-func-css -->)',
-        replace: function(context, match, newline, whitespace, begin, body, end) {
-            var lines = ['dynamic/path-one.css', 'dynamic/path-two.css'].map(function(f) {
-                return whitespace + '<link rel="stylesheet" href="' + f + '">';
-            });
-            // the next line leaves the markers in place; it's easy enough to take them out.
-            return newline + whitespace + begin + lines.join('\n') + end;
-        },
-        opts: {data: {expected: {files: 1, matches: 1}}}
-    }, {
-        tag: 'framework-js',
-        //     1       2           3           4     5
-        re: '^([ \t]*)(<!-- begin:(wf_js) -->)([^]*)(<!-- end:wf_js -->)',
-        replace: htmlScriptReplacement,
-    }, {
-        tag: 'framework-js-vendor',
-        //     1       2           3            4     5
-        re: '^([ \t]*)(<!-- begin:(vendor) -->)([^]*)(<!-- end:vendor -->)',
-        replace: htmlScriptReplacement
-    }, {
-        tag: 'html-design-js',
-        //     1        2                     3     4
-        re: '^([ \t]*)(<!-- begin:design -->)([^]*)(<!-- end:design -->)',
-        replace: function(context, match, whitespace, begin, body, end) {
-            /*
-            var relpath = path.relative(paths.wd_html.dest[target], paths.wd_js.dest[target]);
-
-            var htmlfilename = path.basename(context.file.path);
-            var designScripts = alldesigns[htmlfilename].designScript;
-
-            var lines = designScripts.map(function(f) {
-                f = path.basename(f);
-                return whitespace + '<script src="' + path.join(relpath, f) + '"></script>';
-            });
-            return whitespace + begin + '\n' + lines.join('\n') + '\n' + whitespace + end;
-            // */
-            return '\nTBD\n';
-        },
-    }, {
-        tag: 'js-markers',
-        //     1                  2              x   3
-        re: '^(\\s*)\\/\\/\\+\\+ ([A-Za-z0-9-]+)(?::(.+))* --\\/\\/\\s*$',
-        replace: jsMarkerReplacement
-    }, {
-        tag: 'js-bracketed',
-        //     1                         2              x   3               4
-        re: '(\\s*)\\/\\/\\+\\+ @begin:([A-Za-z0-9-]+)(?::(\\S+))* --\\/\\/([^]*?)\\/\\/\\+\\+ @end:\\2 --\\/\\/',
-        // 1: whitespace
-        // 2: task name
-        // 3: selector
-        // 4: body
-        replace: jsBracketedReplacement
-    }
-];
-
-var markerDefinitionsCount = markerDefinitions.length;
-
-function htmlScriptReplacement(context, match, whitespace, begin, id, body, end) {
-    /*
-    // TODO change .HTML files so this map can be removed
-    //var map = {wf_js: "framework-js", vendor: "framework-js-vendor"};
-    var p = makeTaskPath(context.tag);
-    //console.log('xxx', {match, whitespace, begin, id, body, end});
-    var relpath = path.relative(paths.wd_html.dest[target], paths[p].dest[target]);
-
-    var lines = filenames.get(context.tag).map(function(f) {
-        return whitespace + '<script src="' + path.join(relpath, f) + '"></script>';
-    });
-    return whitespace + begin + "\n" + lines.join("\n") + "\n" + whitespace + end;
-    // */
-    return '\nTBD\n';
-}
-
-function jsMarkerReplacement(context, match, whitespace, task, selector) {
-    /*
-    // TODO kind of kludgy to not reference scripts directory - maybe
-    // remove that from PHP code so I can just remove root? or calculate
-    // relative paths like HTML replace does?
-    var taskpath = makeTaskPath(task);
-    if (!paths[taskpath]) {
-        throw "jsMarkerReplacement can't find task name: '" + task + "'";
-    }
-    taskpath = paths[taskpath].dest[target];
-    var li = taskpath.lastIndexOf('scripts/');
-    taskpath = taskpath.substring(li + 'scripts/'.length);
-    // if no selector it matches everything
-    if (!selector) {
-        selector = '.';
-    }
-    var taskfiles = filenames.get(task);
-    var selected = taskfiles.filter(f => f.search(new RegExp(selector)) > -1);
-    var replacement = match + '\n' + whitespace + "'" + taskpath;
-    return replacement + selected.join("',\n" + whitespace + "'" + taskpath) + "',";
-    // */
-    return '\nTBD\n';
-}
-
-function jsBracketedReplacement(context, match, whitespace, task, selector, body) {
-    /*
-    var taskpath = makeTaskPath(task);
-    if (!paths[taskpath]) {
-        throw "jsBracketedReplacement can't find task name: '" + task + "'";
-    }
-    taskpath = paths[taskpath].dest[target];
-    var li = taskpath.lastIndexOf('scripts/');
-    taskpath = taskpath.substring(li + 'scripts/'.length);
-
-    // if no selector it matches everything
-    if (!selector) {
-        selector = '.';
-    }
-    // the match isn't used here because it spans the body being replaced.
-    // TODO extend RE so it captures prefix and suffix to body, not body.
-    var taskfiles = filenames.get(task);
-    var selected = taskfiles.filter(f => f.search(new RegExp(selector)) > -1);
-    var replacement = whitespace + "'" + taskpath;
-    return replacement + selected.join("',\n" + whitespace + "'" + taskpath) + "',";
-    // */
-    return '\nTBD\n'
-}
-
-// not cloning the objects but getting a new array.
-//var allMarkerDefinitions = markerDefinitions.map(m => m);
 
 function bufferCompare(fixture, expected, transform, done, filename) {
     transform.once('data', function (file) {
@@ -234,42 +81,79 @@ function streamCompare(fixture, expected, transform, done, filename) {
 // run core tests to make sure the constructor works and markers are added correctly
 //
 function runCoreTest() {
+    var markerSets = [];
     var markers, markerTagsDefined;
 
-    describe('Marker consistency checks', function() {
+    // add the markers via objects and arguments.
+    markers = new Markers();
 
-        before(function() {
-            markers = new Markers();
-            markerTagsDefined = markerDefinitions.map(m => m.tag);
+    // add markers both ways here
+    markerDefinitions.forEach(m => markers.addMarker(m));
+    markers.addMarker(
+        'html-copyright-non-object',
+        new RegExp('<!-- insert:html-copyright-non-object:(\\d{4}) -->'),
+        function (context, match, startYear) {
+            var year = new Date().getFullYear();
+            return '<!-- Copyright BAM ' + startYear + '-' + year + ' -->';
+        }
+    );
+    // keep track of tags.
+    markerTagsDefined = markerDefinitions.map(m => m.tag);
+    markerTagsDefined.push('html-copyright-non-object');
+    markerSets.push({markers, markerTagsDefined, msg: "added using addMarker()"});
 
-            // add the markers
-            markerDefinitions.forEach(m => markers.addMarker(m));
+    // now define them using the multiple add function.
+    markers = new Markers();
+    markers.addMarkers(markerDefinitions);
+    markerTagsDefined = markerDefinitions.map(m => m.tag);
+    markerSets.push({markers, markerTagsDefined, msg: "added using addMarkers()"});
 
-            // make sure that a marker can be added as individual arguments
-            markers.addMarker(
-                'html-copyright-non-object',
-                new RegExp('<!-- insert:html-copyright-non-object:(\\d{4}) -->'),
-                function (context, match, startYear) {
-                    var year = new Date().getFullYear();
-                    return '<!-- Copyright BAM ' + startYear + '-' + year + ' -->';
-                }
-            );
-            // add that tag to the array
-            markerTagsDefined.push('html-copyright-non-object');
+    // now define them at construction time
+    markers = new Markers(markerDefinitions);
+    markerTagsDefined = markerDefinitions.map(m => m.tag);
+    markerSets.push({markers, markerTagsDefined, msg: "added using Marker() constructor"});
 
-        });
+    describe('verify internal consistency of Marker class after adding markers', function () {
 
-        it('should have the right number of markers', function() {
-            var markerTags = markers.getMarkerTags();
-            (markerTags.length).should.equal(markerTagsDefined.length);
-        });
+        markerSets.forEach(function(ms) {
+            it("right number of markers when " + ms.msg, function() {
+                var markers = ms.markers;
+                var markerTagsDefined = ms.markerTagsDefined;
 
-        it('should have the right tags', function() {
-            var markerTags = markers.getMarkerTags();
-            markerTags.should.containDeep(markerTagsDefined);
-            markerTagsDefined.should.containDeep(markerTags);
-        });
+                var markerTags = markers.getMarkerTags();
+                (markerTags.length).should.equal(markerTagsDefined.length);
+            });
 
+            it("right tags when " + ms.msg, function() {
+                var markers = ms.markers;
+                var markerTagsDefined = ms.markerTagsDefined;
+
+                var markerTags = markers.getMarkerTags();
+                markerTags.should.containDeep(markerTagsDefined);
+                markerTagsDefined.should.containDeep(markerTags);
+            });
+
+        })
+        /*
+        for (var i = 0; i < markerSets.length; i++) {
+            it('should have the right number of markers', function() {
+                var markers = markerSets[i].markers;
+                var markerTagsDefined = markerSets[i].markerTagsDefined;
+
+                var markerTags = markers.getMarkerTags();
+                (markerTags.length).should.equal(markerTagsDefined.length);
+            });
+
+            it('should have the right tags', function() {
+                var markers = markerSets[i].markers;
+                var markerTagsDefined = markerSets[i].markerTagsDefined;
+
+                var markerTags = markers.getMarkerTags();
+                markerTags.should.containDeep(markerTagsDefined);
+                markerTagsDefined.should.containDeep(markerTags);
+            });
+        };
+        // */
     });
 }
 
